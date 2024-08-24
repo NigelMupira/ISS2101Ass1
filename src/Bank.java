a561d16d97dd7c8581ee6332c2720c2ed4c1eba9
Nigel Mupira <h230828v@hit.ac.zw>
2024-08-24 18:59:44 +0200

    #2

--- src/Bank.java
+++ src/Bank.java
@@ -1,6 +1,19 @@
-// class
+// Class
 public class Bank {
     // Fields
     private String accountType;
     private double accountBalance;
+
+    // Method to deposit money into the account
+    public double deposit(double amount) {
+        // Ensure the deposit amount is positive
+        if (amount > 0) {
+            accountBalance += amount;
+        }
+        else {
+            System.out.println("Invalid amount!");
+        }
+        // Return the updated account balance
+        return accountBalance;
+    }
 }
