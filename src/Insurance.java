b96f97ad4d8e49e11d01c5e9425a5a8fd03d8d36
Nigel Mupira <h230828v@hit.ac.zw>
2024-08-24 19:23:43 +0200

    #6

---
+++ src/Insurance.java
@@ -0,0 +1,7 @@
+public class Insurance extends Bank {
+    // Constructor for the Insurance class
+    public Insurance(String accountType, double accountBalance) {
+        // Call the constructor of the Bank class (superclass)
+        super(accountType, accountBalance);
+    }
+}
